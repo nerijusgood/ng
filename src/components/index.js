56efa331bc42308e429dd5f41d13c64ebb36c6f0
Nerijus Gudas <nerijusgood@gmail.com>
2016-12-29 10:57:27 +0100

    fonts

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export Main from './Main/Main'
 export Navbar from './Navbar/Navbar'
 export Container from './Container/Container'
-// export Profile from './Profile/Profile'
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,3 @@
 export Home from './Home/Home'
 export Landing from './Landing/Landing'
-export Profile from './Profile/Profile'
+// export Profile from './Profile/Profile'
